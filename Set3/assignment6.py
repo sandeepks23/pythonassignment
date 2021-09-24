49ea72f76e4c2e3ad4b32f6077ce5580fd34d052
sandeep <kichusandeep71@gmail.com>
2021-09-24 10:47:14 +0530

    python assignment

--- Set3/assignment6.py
+++ Set3/assignment6.py
@@ -2,7 +2,7 @@ import smtplib
 
 def send_mail():
     server=smtplib.SMTP_SSL("smtp.gmail.com",465)
-    server.login("kichusandeep71@gmail.com","9446328350")
+    server.login("kichusandeep71@gmail.com","PASSWORD")
     server.sendmail("kichusandeep71@gmail.com","kssandeep230@gmail.com","Hello")
     server.quit()
 
